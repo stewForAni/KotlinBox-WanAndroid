c996222fb35d3e66166df08c6a7414eeb694dff0
Stew <>
2022-07-31 20:40:48 +0800

    test kotlin 3

--- kb_home/src/main/java/com/stew/kb_home/ui/HomeFragment.kt
+++ kb_home/src/main/java/com/stew/kb_home/ui/HomeFragment.kt
@@ -20,7 +20,6 @@ class HomeFragment : BaseVMFragment<FragmentHomeBinding>() {
 
     override fun init() {
         homeViewModel.getBanner()
-        homeViewModel.getHomeList()
     }
 
     override fun observe() {
